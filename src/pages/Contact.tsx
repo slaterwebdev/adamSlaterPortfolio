fdba7fa7db6cf139db3e327d3405017f9b966d12
slaterwebdev <adamslater1996@outlook.com>
2024-11-25 00:32:18 +0100

    simplified and refined contact form component to most basic level, ready to implement dynamic input generation now component is fully scoped out

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,80 +1,52 @@
 import { Section, PageTemplate } from "@/components";
 import contactHero from "@/assets/heros/contact.jpg";
 
+const Form = () => {
+  const BASE_INPUT_STYLES = "w-full bg-white mb-4 border-b p-2";
+  return (
+    <div className="form mx-auto mb-12 sm:mb-24 w-full sm:w-[800px] text-black">
+      <input
+        type="text"
+        className={BASE_INPUT_STYLES}
+        placeholder="Enter name"
+        required
+      />
+      <input
+        type="email"
+        className={BASE_INPUT_STYLES}
+        placeholder="Enter email"
+        required
+      />
+      <input
+        type="number"
+        className={BASE_INPUT_STYLES}
+        placeholder="Enter number"
+        required
+      />
+      <textarea
+        className={BASE_INPUT_STYLES}
+        placeholder="Message"
+        id="message"
+        rows={8}
+        required
+      ></textarea>
+
+      <div className="text-right">
+        <button type="button" className="text-primary border-b">
+          Send Enquiry
+        </button>
+      </div>
+    </div>
+  );
+}
+
 export const Contact = () => (
   <PageTemplate content={{ title: "Get In Touch" }} image={contactHero}>
-    <Section title="Send me a message">
-      <p className="text-3xl sm:text-5xl">
+    <Section title="Send me a message" sideContent={<Form/>}>
+      <p className="text-3xl">
         Whether you wish to discuss new ideas or have a project for me, simply
         fill this form and I'll get back to you soon.
       </p>
     </Section>
-    <div className="p-8 rounded-xl shadow-lg max-w-screen-lg mx-auto mb-12 sm:mb-24">
-      <form className="space-y-4">
-        <div className="w-full">
-          <label className="sr-only" htmlFor="name">
-            Name
-          </label>
-          <input
-            className="input input-solid max-w-full"
-            placeholder="Name"
-            type="text"
-            id="name"
-            required
-          />
-        </div>
-
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
-          <div>
-            <label className="sr-only" htmlFor="email">
-              Email
-            </label>
-            <input
-              className="input input-solid"
-              placeholder="Email address"
-              type="email"
-              id="email"
-              required
-            />
-          </div>
-
-          <div>
-            <label className="sr-only" htmlFor="phone">
-              Phone
-            </label>
-            <input
-              className="input input-solid"
-              placeholder="Phone Number"
-              type="tel"
-              id="phone"
-              required
-            />
-          </div>
-        </div>
-
-        <div className="w-full">
-          <label className="sr-only" htmlFor="message">
-            Message
-          </label>
-
-          <textarea
-            className="textarea textarea-solid max-w-full"
-            placeholder="Message"
-            rows={8}
-            id="message"
-            required
-          ></textarea>
-        </div>
-
-        <div className="mt-4">
-          <button
-            type="button"
-            className="rounded-lg btn btn-primary btn-block"
-          >
-            Send Enquiry
-          </button>
-        </div>
-      </form>
-    </div>
   </PageTemplate>
 );
